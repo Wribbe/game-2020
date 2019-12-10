52e4dcfb6c9a52a22875ea9e308ccbfce608fd08
Stefan Eng <eng_steff@hotmail.com>
2019-12-10 19:35:29 +0100

    Load glad correctly to avoid segfault.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//#include "glad/glad.h"
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 int
@@ -13,6 +13,10 @@ main(void)
   if (!glfwInit())
     return -1;
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window)
@@ -23,6 +27,11 @@ main(void)
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    fprintf(stderr, "[ERROR:] Failed to initialize GLAD.\n");
+    return -1;
+  }
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
