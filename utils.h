79248d8f79af0c6f602ffe15af4b9ff7db0451c5
Stefan Eng <eng_steff@hotmail.com>
2019-12-11 23:46:39 +0100

    Window is displayed for X11.

--- utils.h
+++ utils.h
@@ -5,12 +5,48 @@
 
 #include <stdint.h>
 
+#include <X11/Xlib.h>
+
+struct xxWindow_native {
+  Display * display;
+  Window window;
+  XEvent event;
+  int screen;
+};
+
 struct xxWindow {
   const char * name;
   uint32_t width;
   uint32_t height;
   bool open;
+  struct xxWindow_native native;
 };
+
+void
+xxWindow_native_create(struct xxWindow * window)
+{
+  struct xxWindow_native * native = &window->native;
+  native->display = XOpenDisplay(NULL);
+  if (native->display == NULL) {
+    printf("Could not open display!\n");
+  }
+  native->screen = DefaultScreen(native->display);
+  native->window = XCreateSimpleWindow(
+    native->display,
+    RootWindow(native->display, native->screen),
+    10,
+    10,
+    window->height,
+    window->width,
+    1,
+    BlackPixel(native->display, native->screen),
+    WhitePixel(native->display, native->screen)
+  );
+  XSelectInput(native->display, native->window, ExposureMask | KeyPressMask);
+  XMapWindow(native->display, native->window);
+
+  XNextEvent(native->display, &native->event);
+}
 
 enum XX_KEY {
   XX_ESC,
@@ -22,7 +58,9 @@ bool xx_state_keys_releases[XX_NUM_KEYS];
 struct xxWindow
 xxwindow_get(const char * name, uint32_t width, uint32_t height)
 {
-  return (struct xxWindow){name, width, height, true};
+  struct xxWindow window = {name, width, height, true};
+  xxWindow_native_create(&window);
+  return window;
 }
 
 void
