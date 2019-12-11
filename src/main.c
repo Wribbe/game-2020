84f3f49a2e04e81df48b2164d0e287a67f8b5d95
Stefan Eng <eng_steff@hotmail.com>
2019-12-11 23:10:13 +0100

    Continue sketching on possible window-api.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,11 @@ int
 main(void)
 {
   struct xxWindow window = xxwindow_get("name", 600, 800);
-  while (xxwindow_isopen(&window)) {
+  while (xxwindow_open_get(&window)) {
+    xxinput_refresh();
+    if (xxinput_released(XX_ESC)) {
+      xxwindow_open_set(&window, false);
+    }
   }
   xxwindow_terminate(&window);
 }
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <stdint.h>
 
@@ -8,12 +9,20 @@ struct xxWindow {
   const char * name;
   uint32_t width;
   uint32_t height;
+  bool open;
 };
+
+enum XX_KEY {
+  XX_ESC,
+  XX_NUM_KEYS,
+};
+
+bool xx_state_keys_releases[XX_NUM_KEYS];
 
 struct xxWindow
 xxwindow_get(const char * name, uint32_t width, uint32_t height)
 {
-  return (struct xxWindow){name, width, height};
+  return (struct xxWindow){name, width, height, true};
 }
 
 void
@@ -21,8 +30,25 @@ xxwindow_terminate(struct xxWindow * window)
 {
 }
 
-int
-xxwindow_isopen(struct xxWindow * window)
+bool
+xxwindow_open_get(struct xxWindow * window)
 {
-  return 1;
+  return window->open;
+}
+
+void
+xxwindow_open_set(struct xxWindow * window, bool state)
+{
+  window->open = state;
+}
+
+void
+xxinput_refresh(void)
+{
+}
+
+bool
+xxinput_released(enum XX_KEY key)
+{
+  return xx_state_keys_releases[key];
 }
