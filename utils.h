d0aac98983f3e7921364badab206360aaf6477e8
Stefan Eng <eng_steff@hotmail.com>
2019-12-13 00:32:47 +0100

    Getting 'HELLO' from input thread on input.

--- utils.h
+++ utils.h
@@ -40,12 +40,31 @@ struct xxWindow {
   bool open;
   thrd_t thread_input;
   uint8_t index_event_first_empty;
+  mtx_t mutex;
+  cnd_t condition;
   struct xxEvent buffer_events[BUFFER_EVENTS_SIZE];
   bool state_keys_released[XX_NUM_KEYS];
   bool state_keys_pressed[XX_NUM_KEYS];
   bool state_keys_held[XX_NUM_KEYS];
   struct xxWindow_native native;
 };
+
+int
+handler_input(void * data)
+{
+  struct xxWindow * window = (struct xxWindow *)data;
+  Display * display = window->native.display;
+  XEvent * event = &window->native.event;
+  mtx_lock(&window->mutex);
+  printf("%s\n","INIT FROM THREAD");
+  cnd_signal(&window->condition);
+  printf("%s\n","SIGNAL DONE!");
+  while (window->open) {
+    XNextEvent(display, event);
+    printf("%s\n","HELLO");
+  }
+  return 0;
+}
 
 void
 xxwindow_native_create(struct xxWindow * window)
@@ -69,8 +88,12 @@ xxwindow_native_create(struct xxWindow * window)
   );
   XSelectInput(native->display, native->window, ExposureMask | KeyPressMask);
   XMapWindow(native->display, native->window);
+}
 
-  XNextEvent(native->display, &native->event);
+void
+xxwindow_native_destroy(struct xxWindow * window)
+{
+  XCloseDisplay(window->native.display);
 }
 
 struct xxWindow
@@ -78,30 +101,23 @@ xxwindow_get(const char * name, uint32_t width, uint32_t height)
 {
   struct xxWindow window = {name, width, height, true};
   xxwindow_native_create(&window);
+  mtx_init(&window.mutex, mtx_plain);
+  cnd_init(&window.condition);
+  thrd_create(&window.thread_input, handler_input, (void *)&window);
+  mtx_lock(&window.mutex);
+  while (true) {
+    if (cnd_wait(&window.condition, &window.mutex) == thrd_success) {
+      break;
+    }
+  }
+  printf("Window done!\n");
   return window;
-}
-
-void
-xxwindow_terminate(struct xxWindow * window)
-{
-  (void)window;
-}
-
-bool
-xxwindow_open_get(struct xxWindow * window)
-{
-  return window->open;
-}
-
-void
-xxwindow_open_set(struct xxWindow * window, bool state)
-{
-  window->open = state;
 }
 
 void
 xxinput_flush(struct xxWindow * window)
 {
+  mtx_lock(&window->mutex);
   for (int i=0; i<XX_NUM_KEYS; i++) {
     window->state_keys_released[i] = false;
     window->state_keys_pressed[i] = false;
@@ -121,7 +137,37 @@ xxinput_flush(struct xxWindow * window)
         break;
     }
   }
+  window->index_event_first_empty = 0;
+  printf("UNLOCKING\n");
+  mtx_unlock(&window->mutex);
+  printf("DONE\n");
 }
+
+bool
+xxwindow_open_get(struct xxWindow * window)
+{
+  return window->open;
+}
+
+void
+xxwindow_open_set(struct xxWindow * window, bool state)
+{
+  mtx_lock(&window->mutex);
+  window->open = state;
+  mtx_unlock(&window->mutex);
+}
+
+void
+xxwindow_terminate(struct xxWindow * window)
+{
+  if (xxwindow_open_get(window)) {
+    xxwindow_open_set(window, false);
+  }
+  thrd_join(window->thread_input, NULL);
+  mtx_destroy(&window->mutex);
+  xxwindow_native_destroy(window);
+}
+
 
 bool
 xxinput_released(struct xxWindow * window, enum XX_KEY key)
